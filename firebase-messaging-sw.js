1cd978db7f4c21e5a3ee61977f7f16324fd90b73
gyanu <gyanu.chauhan@sveltetech.com>
2024-09-17 14:07:49 +0530

    config js files are commited

---
+++ firebase-messaging-sw.js
@@ -0,0 +1,33 @@
+importScripts("https://www.gstatic.com/firebasejs/8.8.0/firebase-app.js");
+importScripts("https://www.gstatic.com/firebasejs/8.8.0/firebase-messaging.js");
+
+const firebaseConfig = {
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
+  measurementId: ""
+};
+
+firebase.initializeApp(firebaseConfig);
+const messaging = firebase.messaging();
+
+messaging.onBackgroundMessage(function (payload) {
+  console.log(
+    "[firebase-messaging-sw.js] Received background message ",
+    payload
+  );
+  // Customize notification here
+  const notificationTitle = payload.notification.title;
+  const notificationOptions = {
+    body: payload.notification.body,
+    // icon: "/firebase-logo.png",
+  };
+
+  return self.registration.showNotification(
+    notificationTitle,
+    notificationOptions
+  );
+});
