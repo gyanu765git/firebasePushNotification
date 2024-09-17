69695d48127ba2b0f5232ebd9f9217ab5016739d
gyanu <gyanu.chauhan@sveltetech.com>
2024-09-17 15:20:28 +0530

    README.md file updated

--- notifications/api/v1/views.py
+++ notifications/api/v1/views.py
@@ -15,7 +15,6 @@ class sendNotification(APIView):
         }
         token = "cYWeXD9LQGBEGC6lPkD3LM:APA91bGDUYhCn53T3sLFXB3OPuNPw_tUviUYS5Wu0O1wMdSrXkUtid9HwVyh0Q4pxUHzZNg8P35sbnavOAu3JWz32__pB19i2sL0zz1kb5n3Ptuix87SCCWiXlDE8tniMEIp3uZV76zk"
         
-        # Create the message to send
         message = messaging.Message(
             notification=messaging.Notification(
                 title=notification['title'],
@@ -25,12 +24,10 @@ class sendNotification(APIView):
             token=token,
         )
         try:
-            # Send the notification
             messaging.send(message)
             print("Notification sent successfully")
             return Response({"success": True, "message": "Notification sent successfully", "data": []},
                             status=status.HTTP_200_OK)
         except Exception as e:
-            # Return error response if any issue occurs
             return Response({"success": False, "message": str(e), "data": []},
                             status=status.HTTP_500_INTERNAL_SERVER_ERROR)
